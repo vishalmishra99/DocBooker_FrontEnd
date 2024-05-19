2583a3946b9ca9305f600bb78014211a762e2b3d
vishalmishra99 <132979738+vishalmishra99@users.noreply.github.com>
2024-05-19 18:40:31 +0530

    Update MessageForm.jsx

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -13,7 +13,7 @@ const MessageForm = () => {
     e.preventDefault();
     try {
       await axios.post(
-        "http://localhost:4000/api/v1/message/send",
+        "https://docbooker-backend-fpxs.onrender.com/api/v1/message/send",
         { firstName, lastName, phone, email, message },
         {
           withCredentials: true,
