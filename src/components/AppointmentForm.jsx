a3b219fa3004f25c8966f4ab495d9e742414663f
vishalmishra99 <132979738+vishalmishra99@users.noreply.github.com>
2024-05-20 17:41:56 +0530

    Update AppointmentForm.jsx

--- src/components/AppointmentForm.jsx
+++ src/components/AppointmentForm.jsx
@@ -36,7 +36,7 @@ const AppointmentForm = () => {
   useEffect(() => {
     const fetchDoctors = async () => {
       const { data } = await axios.get(
-        "https://docbooker-backend.onrender.com/api/v1/user/doctors",
+        "https://docbooker-backend-2.onrender.com/api/v1/user/doctors",
         { withCredentials: true }
       );
       setDoctors(data.doctors);
