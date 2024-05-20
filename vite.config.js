9f68afa863dde501ec604077c8d012221c76be1c
vishalmishra99 <132979738+vishalmishra99@users.noreply.github.com>
2024-05-20 16:57:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/api':{
-        port:3000,
-        target: 'https://docbooker-backend.onrender.com',
-        changeOrigin:true,
-      }
-    }
-  }
 })
